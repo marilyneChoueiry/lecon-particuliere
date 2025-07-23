fee4b4838c69928c613e441f0eed4d3a8b4bb7ab
cyrine mallah <cyrinemallah06@gmail.com>
2025-07-23 20:10:39 +0300

    Add files via upload

--- register_enseignant.php
+++ register_enseignant.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/../function.php';
 if($_SERVER["REQUEST_METHOD"] != 'POST'){
     die
 
@@ -66,4 +67,39 @@ if($_SERVER["REQUEST_METHOD"] != 'POST'){
     if($_FILES["cv"]["error"] !== 0) die
      if($_FILES["cv"]["type"] != 'application/pdf') die
      if($_FILES["cv"]["size"] > (5 * 1024 * 1024)) die
- 
+  try {
+        
+        $conn=getPdoConnention(); 
+        if (!$conn) {
+    die(" Database connection failed — \$conn is null");
+}
+        $checkStmt = $conn->prepare("SELECT COUNT(*) FROM users WHERE email = ?");
+        $checkStmt->execute([$email]);
+        $emailExists = $checkStmt->fetchColumn();
+
+    if ($emailExists) {
+        echo "⚠️ This email is already registered.";
+         exit;
+}
+
+        $query = "INSERT INTO users (Firstname, Lastname, email, password, region, cycle, description,cv)
+        VALUES (:name, :lastname, :email, :password, :region, :cycle, :role)";
+        $stmt = $conn->prepare($query);
+        
+        $stmt->bindParam(':Firstname', $Firstname);
+        $stmt->bindParam(':Lastname', $Lastname);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $password); 
+        $stmt->bindParam(':region', $region);
+        $stmt->bindParam(':cycle', $cycle);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':cv', $cv);
+        
+        $stmt->execute();
+
+        echo " User inserted successfully!";
+    } catch (PDOException $e) {
+        echo " PDO Error: " . $e->getMessage();
+    }
+    
+}
